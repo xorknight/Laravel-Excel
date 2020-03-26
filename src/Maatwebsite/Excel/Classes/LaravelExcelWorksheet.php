0fb56d348543627986d197cd9fe555b9f13293a1
lande backekolo <backekolol@mlp.agency>
2020-03-26 09:20:18 +0000

    Changed string helper

--- src/Maatwebsite/Excel/Classes/LaravelExcelWorksheet.php
+++ src/Maatwebsite/Excel/Classes/LaravelExcelWorksheet.php
@@ -718,7 +718,7 @@ class LaravelExcelWorksheet extends PHPExcel_Worksheet {
         $caller = $this->getDefaultStyle();
         $params = is_array($params) ? $params : [$params];
 
-        if (str_contains($key, 'font'))
+        if (\Str::contains($key, 'font'))
             return $this->setFontStyle($caller, $setter, $key, $params);
 
         return call_user_func_array([$caller, $setter], $params);
